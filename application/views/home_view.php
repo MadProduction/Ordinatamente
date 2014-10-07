5989d3148cf7a58e69f483ad0fccf974557966a1
MadProduction <info@madproduction.it>
2014-10-07 22:02:37 +0200

    scritta ciao ciao

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -9,6 +9,7 @@
         
           <div class="col-sm-6">
               <img width="70%" src="/style/img/ord_logo_grey.png">
+              ciao ciao
               
               <div class="freccia">
                   
