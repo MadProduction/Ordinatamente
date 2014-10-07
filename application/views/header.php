77847d1b6048e5b993078d4b6aa4fb5b4484acc3
MadProduction <info@madproduction.it>
2014-10-07 22:26:19 +0200

    - cambio view
    - generazione css Anna

--- application/views/header.php
+++ application/views/header.php
@@ -1,21 +1,15 @@
 <html>
 <head>
     <title><?php echo $title; ?></title>
-    <link href="http://dev.ordinatamente.com/style/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="http://dev.ordinatamente.com/style/css/stili-custom.css" rel="stylesheet" type="text/css">
+    <link href="http://dev.ordinatamente.com/style/css/style_anna.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     
-    <div class="container">
-        
-         <div class="row spessore">
-              <div class="col-sm-3">
-      
-             </div>
-              <div class="col-sm-6">
-       
-             </div>
-             <div class="col-sm-3" id="destra">
-        IT | EN
-             </div>
+    <div class="lang">
+        <nav>
+            <ul>
+                <li>IT</li>
+                <li>EN</li>
+            </ul>
+        </nav>
      </div>
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,34 +1,8 @@
-
-    
-<div class="container"> 
-    <div class="row">
-        
-          <div class="col-sm-3">
-      
-             </div>
-        
+<div class="container">      
           <div class="col-sm-6">
               <img width="70%" src="/style/img/ord_logo_grey.png">
-              
-              <div class="freccia">
-                  
-                  <img src="/style/img/arrow_down.png">
-                  
-              </div>
-              
              </div>
-        
-        
-          <div class="col-sm-3">
-      
-             </div>
-                
-    
-    </div>
-    
-
-    
-</div>                   
+</div>                
 
    
     
