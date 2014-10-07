fa63e0e17b5742eff2fc2a3eef165b9601f5343a
Beppe <Beppe@beppes-mbp.station>
2014-10-07 23:17:10 +0200

    Era Legis

--- application/views/header.php
+++ application/views/header.php
@@ -2,12 +2,15 @@
 <head>
     <title><?php echo $title; ?></title>
     <link href="http://dev.ordinatamente.com/style/css/style_anna.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js""></script>
+    <script src="http://date.lashtal.com/eralegis.js"></script>
 </head>
 <body>
     
     <div class="lang">
         <nav>
             <ul>
+                <li><span data-evdate="<?php echo date('Y-m-d H:i:s');?>" class="date93"></span></li>
                 <li>IT</li>
                 <li>EN</li>
             </ul>
